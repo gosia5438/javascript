b3f92ecc3546e395099cddebf1fb3f79876115f1
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2017-12-17 12:17:17 +0100

    typy zmiennych

---
+++ podstawy/js/5-datatypes.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//TYP LICZBOWY
+var wyplata = 5;
+var premia =2500;
+var calkowitaWyplata;
+calkowitaWyplata = wyplata + premia;
+console.log(calkowitaWyplata);
+
+
+//STRING
+var wyplataStr = "Adam ma \" 5000";
+var premiaStr = "2500";
+var calkowitaWyplataStr;
+calkowitaWyplataStr = wyplataStr + premiaStr;
+console.log (calkowitaWyplataStr);
+
+
+//TYPY LOGICZNE
+var czyJestSmog;
+czyJestSmog = true;
+
+if (czyJestSmog) {
+    console.log("Jest SMOG");
+    } else {
+        console.log("Nie ma SMOGa;)");
+    }
+
+
+//TYPE SPECJALNE - UDENFINED
+
+
