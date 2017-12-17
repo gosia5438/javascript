5ba2f4a72cea1d5af5b913f5a514a714f37b8166
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2017-12-17 15:00:20 +0100

    operatory matematyczne

--- podstawy/js/8-object-math.js
+++ podstawy/js/8-object-math.js
@@ -13,12 +13,3 @@ console.log(Modulo);
 liczba1 +=liczba2; //to samo co liczba1= liczba1+liczba2
 console.log(liczba1);
 
-if ('2' === 2) {
-    console.log ('true');
-} else {
-    console.log ('false');
-}
-
-if (!(('2' !== 2) && (8<4))){
-    document.write('<br> prawda')
-}
