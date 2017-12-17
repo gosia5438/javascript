10de603ae1c8f1073f851d22de75a6985f724465
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2017-12-17 09:53:06 +0100

    zmienne- variables

---
+++ podstawy/js/2-variables.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var imie;
+var imieDamskie = 'Kaja';
+var wiek = 37;
+
+var kolor = 'czerwony';
+
+imie = 'Sergio';
+imie = imieDamskie;  //imie = Kaja
+
+console.log(imie);
+console.log(kolor);
+console.log(wiek);
+//Console logi musza byc po zdefiniowaniu zmiennych var 
+
+
+
+//zeby sprawdzic czy plik jest przypiety console.log("Gosia");
