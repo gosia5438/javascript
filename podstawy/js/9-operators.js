abf16d89fa24f1afdf39e094974ff64fad26b955
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2017-12-17 15:00:31 +0100

    operatory

---
+++ podstawy/js/9-operators.js
@@ -0,0 +1,12 @@
+if ('2' === 2) {
+    'use strict';
+    console.log ('true');
+} else {
+    console.log ('false');
+}
+
+if (!(('2' !== 2) && (8<4))){
+    document.write('<br> prawda')
+}
+
+(8>9) ? console.log('true') : console.error(false);
