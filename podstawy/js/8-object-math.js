b7d1380bd301e329fa332c46d250b595b5183400
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2017-12-17 14:55:26 +0100

    objekty matematyczne

---
+++ podstawy/js/8-object-math.js
@@ -0,0 +1,24 @@
+'use strict';
+console.log( Math.PI);
+console.log( Math.cos);
+
+
+//modulo
+var liczba1 = 5;
+var liczba2 = 13;
+var Modulo = liczba2%liczba1;
+
+console.log(Modulo);
+
+liczba1 +=liczba2; //to samo co liczba1= liczba1+liczba2
+console.log(liczba1);
+
+if ('2' === 2) {
+    console.log ('true');
+} else {
+    console.log ('false');
+}
+
+if (!(('2' !== 2) && (8<4))){
+    document.write('<br> prawda')
+}
