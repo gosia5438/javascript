fce4f4a28b9897fb17f1883b75659002a16f1c34
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2018-01-13 12:29:42 +0100

    manipulacje w css

---
+++ DOM/podstawy/js/4_manipulacja-dom-html.js
@@ -0,0 +1,11 @@
+'use strict';
+var mainHeader = document.querySelector('h1');
+mainHeader.innerHTML = 'Nowa tresc naglowka';
+var pobierzTresc = mainHeader.innerHTML;
+//console.log(pobierzTresc);
+
+var link = document.querySelectorAll('a')[0];
+link.href = '#';
+link.className = 'nowa-klasa';
+
+mainHeader.style.color = '#11aa22';
