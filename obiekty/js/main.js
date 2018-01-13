8cfa85b837728d71a960153ac9c118dbac6ac49b
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2018-01-13 14:56:22 +0100

    obiekty

--- obiekty/js/main.js
+++ obiekty/js/main.js
@@ -1,5 +1,41 @@
 'use strict';
-class Ogloszenie { //nazwy class z duzej litery !
+
+class Ogloszenie {
+    constructor(tytul, opis, kategoria, status, cena) {
+        this.tytul = tytul;
+        this.opis = opis;
+        this.kategoria = kategoria;
+        this.status = status;
+        this.cena = cena;
+    }
+    pobierzTytul (){
+        return this.tytul;
+    }        
+}
+ var ogloszenie1 = new Ogloszenie('Sprzedam Opla', 'Kombi 2009 w dobrym stanie', 'motoryzacja', 'aktywne', 15000);
+var ogloszenie2= new Ogloszenie('Sprzedam Passata', 'Igła, nie bity od Niemca', 'motoryzacja','nieaktywne',5000 );
+var ogloszenie3 = new Ogloszenie('Mieszkanie M4', 'super lokalizacja', 'nieruchomości', 'aktywne', 500000);
+//console.log(ogloszenie1);
+//console.log(ogloszenie2);
+//console.log(ogloszenie3);
+//
+//console.log(ogloszenie2.pobierzTytul());
+
+var arr = [
+    {tytul: 'w pustyni i w puszczy', stan:true},
+    {tytul: 'rura', stan: false}
+];
+for (var i = 0; i < arr.length; i++){
+    if(arr[i].stan == true){
+        console.log('hura');
+    } else {
+        console.log('nie hura');
+    }
+}
+
+
+
+/*class Ogloszenie { //nazwy class z duzej litery !
     constructor(tytul, cena, opis) {
         this.tytul = tytul;
         this.cena = cena;
@@ -20,4 +56,4 @@ console.log(ogloszenie1);
 var ogloszenie2 = new Ogloszenie('Sweter', 50, 'Cieply sweter');
 var tytul = ogloszenie2.pobierzTytul();  //lub 
 //var tytul = ogloszenie2.tytul(); 
-console.log(tytul);
+console.log(tytul);*/
