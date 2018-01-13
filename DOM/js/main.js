5c35c3b7f27f4495eb84227207ef170629f8390e
Malgorzata Maciejowska <malgorzatamaciejowska21@gmail.com>
2018-01-13 10:24:23 +0100

    pobieranie elementow ze strony HTML

---
+++ DOM/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+var parFirst = document.getElementById('parFirst');
+var linkiPoTagu = document.getElementsByTagName('a');
+var linki = document.getElementsByClassName('superlink');
+var divPoId = document.querySelector('#parSecond');   // przy query trzeba pobierac wraz z # i .
+var superlink = document.querySelectorAll('.superLink'); 
+console.log(superlink);
